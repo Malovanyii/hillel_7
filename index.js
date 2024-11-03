953a723b80de8d953c13043a08d36d0f3e7b5d9a
Malovanyi <malovaniyy@gmail.com>
2024-11-03 19:35:43 +0200

    7.2

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-// ДЗ 7.1. Замикання
-function multiply(a) {
-    let result = 0;
+// // ДЗ 7.1. Замикання
+// function multiply(a) {
+//     let result = 0;
+//     return function (b) {
+//         result += b;
+//         return result;
+//     }
+// }
+
+// const sum = multiply();
+
+// console.log(sum(4));
+// console.log(sum(6));
+// console.log(sum(10));
+// console.log(sum(7));
+
+// ДЗ 7.2. Добуток через карування
+function multiplyCurry(a) {
     return function (b) {
-        result += b;
-        return result;
-    }
+        return a + b;
+    };
 }
 
-const sum = multiply();
-
-console.log(sum(4));
-console.log(sum(6));
-console.log(sum(10));
-console.log(sum(7));
+console.log(multiplyCurry(5)(2));
